7c2496ee3a219acf2831494958cfc8c4e30df86f
MarcosD00 <olito007.23@gmail.com>
2024-07-06 23:16:58 -0500

    cleaning

--- advanced_search_results.php
+++ advanced_search_results.php
@@ -48,7 +48,6 @@ if($wpestate_prop_unit=='list'){
     $align_class=   'the_list_view';
 }
 
-
 $adv_search_what    =   wpresidence_get_option('wp_estate_adv_search_what','');
 $adv_search_how     =   wpresidence_get_option('wp_estate_adv_search_how','');
 $adv_search_label   =   wpresidence_get_option('wp_estate_adv_search_label','');
