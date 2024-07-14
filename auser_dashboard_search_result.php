f20bb02b6b93ba77ff7ed28f3f3a4d5df023be60
MarcosD00 <olito007.23@gmail.com>
2024-07-13 22:10:04 -0500

    cleaning

--- auser_dashboard_search_result.php
+++ auser_dashboard_search_result.php
@@ -18,7 +18,6 @@ $agent_list                     =   (array)get_user_meta($userID,'current_agent_
 $wpestate_options               =   wpestate_page_details($post->ID);
 
 
-
 if ( ! isset( $_POST['dashboard_searches_nonce'] )  || ! wp_verify_nonce( $_POST['dashboard_searches_nonce'], 'dashboard_searches' ) ) {
     esc_html_e('Sorry, your nonce did not verify.','wpresidence');
     exit;
