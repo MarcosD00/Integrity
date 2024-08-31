d5fd198ae4cf393ba89b02ae379fad9b337bf09f
MarcosD00 <olito007.23@gmail.com>
2024-08-30 23:51:55 -0500

    cleaning

--- compare_listings.php
+++ compare_listings.php
@@ -23,7 +23,6 @@ foreach ($_POST['selected_id'] as $key => $value) {
     $list_prop[] = $value;
 }
 
-
 $unit           =   esc_html ( wpresidence_get_option('wp_estate_measure_sys', '') );
 $wpestate_currency       =   esc_html ( wpresidence_get_option('wp_estate_currency_symbol', '') );
 $where_currency =   esc_html ( wpresidence_get_option('wp_estate_where_currency_symbol', '') );
